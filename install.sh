40a6f1090c1c349a019bb1d4387a1a77c3c22854
drosofff <drosofff@gmail.com>
2018-02-13 23:46:30 +0100

    streamline specific set of metavisitor files in the repo

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-git clone https://github.com/ARTbio/GalaxyKickStart.git -b metavisitor-2.0
+git clone https://github.com/ARTbio/GalaxyKickStart.git
 
 
 cd GalaxyKickStart/
