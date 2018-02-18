78806240c38bae460a1afa1bf39e1455f7f49bee
drosofff <drosofff@gmail.com>
2018-02-18 23:16:24 +0100

    build full metavisitor docker image in a third independent job and push to artbio/metavisitor-2-beta docker hub

--- travis_push_docker.sh
+++ travis_push_docker.sh
@@ -1,7 +1,35 @@
+#!/usr/bin/env bash
+set -e
+
+# build full metavisitor image
+apt-get install -y python-pip python-dev python-setuptools git htop
+echo "Upgrading pip to v 1.9"
+pip install -U pip
+pip --version
+/usr/local/bin/pip install ansible==2.2
+ansible --version
+
+git clone https://github.com/ARTbio/GalaxyKickStart.git
+rm -rf GalaxyKickStart/Dockerfile GalaxyKickStart/Dockerfile.test
+mv Dockerfile Dockerfile.test GalaxyKickStart/
+rm -rf GalaxyKickStart/group_vars/metavisitor GalaxyKickStart/group_vars/test 
+mv group_vars/metavisitor group_vars/test GalaxyKickStart/group_vars/
+rm -rf GalaxyKickStart/extra-files/metavisitor GalaxyKickStart/extra-files/test
+mv extra-files/metavisitor extra-files/test GalaxyKickStart/extra-files/
+rm -rf GalaxyKickStart/inventory_files/*
+mv inventory_files/metavisitor inventory_files/test GalaxyKickStart/inventory_files/
+
+cd GalaxyKickStart/
+ansible-galaxy install -r requirements_roles.yml -p roles/ -f
+echo "Editing group_vars/all"
+sed -i -e 's/galaxy_manage_trackster: true/galaxy_manage_trackster: false/' group_vars/all
+docker build -t metavisitor .
+
+
 # Next we upload to docker
-docker tag metavisitor artbio/metavisitor-2.0:$TRAVIS_COMMIT
-docker tag metavisitor artbio/metavisitor-2.0:latest
+docker tag metavisitor artbio/metavisitor-2-beta:$TRAVIS_COMMIT
+docker tag metavisitor artbio/metavisitor-2-beta:latest
 LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
 $LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
-docker push artbio/metavisitor-2.0:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
-docker push artbio/metavisitor-2.0:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
+docker push artbio/metavisitor-2-beta:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
+docker push artbio/metavisitor-2-beta:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
