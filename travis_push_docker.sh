012ea0f344403dc0710232e3c54249a05d9a0d62
drosofff <drosofff@gmail.com>
2018-02-18 23:29:06 +0100

    change .travis.yml and travis_push_docker.sh for conditional execution of jobs

--- travis_push_docker.sh
+++ travis_push_docker.sh
@@ -2,27 +2,6 @@
 set -e
 
 # build full metavisitor image
-apt-get install -y python-pip python-dev python-setuptools git htop
-echo "Upgrading pip to v 1.9"
-pip install -U pip
-pip --version
-/usr/local/bin/pip install ansible==2.2
-ansible --version
-
-git clone https://github.com/ARTbio/GalaxyKickStart.git
-rm -rf GalaxyKickStart/Dockerfile GalaxyKickStart/Dockerfile.test
-mv Dockerfile Dockerfile.test GalaxyKickStart/
-rm -rf GalaxyKickStart/group_vars/metavisitor GalaxyKickStart/group_vars/test 
-mv group_vars/metavisitor group_vars/test GalaxyKickStart/group_vars/
-rm -rf GalaxyKickStart/extra-files/metavisitor GalaxyKickStart/extra-files/test
-mv extra-files/metavisitor extra-files/test GalaxyKickStart/extra-files/
-rm -rf GalaxyKickStart/inventory_files/*
-mv inventory_files/metavisitor inventory_files/test GalaxyKickStart/inventory_files/
-
-cd GalaxyKickStart/
-ansible-galaxy install -r requirements_roles.yml -p roles/ -f
-echo "Editing group_vars/all"
-sed -i -e 's/galaxy_manage_trackster: true/galaxy_manage_trackster: false/' group_vars/all
 docker build -t metavisitor .
 
 
