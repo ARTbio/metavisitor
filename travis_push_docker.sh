0e91e90f1e27d290332bc61ef129d8fbe6da7bb3
Christophe Antoniewski <drosofff@gmail.com>
2018-02-21 19:10:39 +0100

    remove the deploy stage and send the docker image in the script section

--- travis_push_docker.sh
+++ travis_push_docker.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -e
-if [ "$TRAVIS_JOB" == "build-docker" ]; then
-    # Next we upload to docker
-    LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
-    $LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
-    docker push artbio/metavisitor-2-beta:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
-    docker push artbio/metavisitor-2-beta:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
-fi
+# Next we upload to docker
+LOGIN="docker login -e=$DOCKER_EMAIL -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
+$LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
+docker push artbio/metavisitor-2-beta:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/galaxy-kickstart-base:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
+docker push artbio/metavisitor-2-beta:latest || (sleep 5s && docker push artbio/galaxy-kickstart-base:latest || echo "push failed twice, quitting" && exit 1)
--- travis_test.sh
+++ travis_test.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 set -e
-curl --fail ${BIOBLEND_GALAXY_URL}/api/version
-date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
-sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
-  cd $GALAXY_HOME &&
-  bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
+if [ $TRAVIS_JOB = "docker" ] || [ $TRAVIS_JOB = "ansible" ]; then
+    echo "Bioblend Testing"
+    curl --fail ${BIOBLEND_GALAXY_URL}/api/version
+    date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
+    sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
+        cd $GALAXY_HOME &&
+        bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
+if
+
+if [ $TRAVIS_JOB = "build-docker" ]; then
+    echo "skipping Bioblend testing"
+    echo "pushing docker image to docker hub"
+    bash $TRAVIS_BUILD_DIR/travis_push_docker.sh
+fi
