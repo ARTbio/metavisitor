15d9353f58cf5dea400c08c37f00f8d847e881d7
drosofff <drosofff@gmail.com>
2018-02-22 00:17:22 +0100

    no xor in boolean bash

--- travis_test.sh
+++ travis_test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 echo "$TRAVIS_JOB has been received"
-if ([[ $TRAVIS_JOB = "docker" ]] || [[ $TRAVIS_JOB = "ansible" ]]); then
+
+if [[ $TRAVIS_JOB = "docker" ]]; then
     echo "Bioblend Testing"
     curl --fail ${BIOBLEND_GALAXY_URL}/api/version
     date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
@@ -9,6 +10,16 @@ if ([[ $TRAVIS_JOB = "docker" ]] || [[ $TRAVIS_JOB = "ansible" ]]); then
         cd $GALAXY_HOME &&
         bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
 if
+
+if [[ $TRAVIS_JOB = "ansible" ]]; then
+    echo "Bioblend Testing"
+    curl --fail ${BIOBLEND_GALAXY_URL}/api/version
+    date > $HOME/date.txt && curl --fail -T $HOME/date.txt ftp://localhost:21 --user $GALAXY_USER:$GALAXY_USER_PASSWD
+    sudo -E su $GALAXY_TRAVIS_USER -c "export PATH=$GALAXY_HOME/.local/bin/:$PATH &&
+        cd $GALAXY_HOME &&
+        bioblend-galaxy-tests -v $GALAXY_HOME/.local/lib/python2.7/site-packages/bioblend/_tests/TestGalaxy*.py"
+if
+
 
 if [ $TRAVIS_JOB = "build-docker" ]; then
     echo "skipping Bioblend testing"
