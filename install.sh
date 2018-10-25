7d1eeb7c9e01a3cc5d03a5055f159290ba27f80f
ccedmendoza <cedricmendoza@gmail.com>
2018-10-25 14:42:11 +0000

    Resolve CryptographyDeprecationWarning

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ apt-get install -y python-pip python-dev python-setuptools git htop
 echo "Upgrading pip to v 1.9"
 pip install -U pip
 pip --version
+/usr/local/bin/pip install cryptography==2.2.2
 /usr/local/bin/pip install ansible==2.2
 ansible --version
 
