8b58633a3897680a9746e05e207c10de9639129f
drosofff <drosofff@gmail.com>
2019-07-18 16:06:10 +0200

    push docker image to docker registry

---
+++ push_to_docker_registry.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+# we have to push the docker image because the galaxykickstart context for the Dockerfile
+# is buit dynamically
+
+if [ $TRAVIS_JOB = "docker" ] && [ "${TRAVIS_BRANCH}" = "master" ]; then
+    echo "pushing docker image to https://cloud.docker.com/u/artbio/repository/docker/artbio/metavisitor-2"
+    LOGIN="docker login -u=$DOCKER_USERNAME -p=$DOCKER_PASSWORD"
+    $LOGIN || (sleep 5s && $LOGIN || echo "login failed twice, quitting" && exit 1)
+    docker push artbio/metavisitor-2:$TRAVIS_COMMIT || (sleep 5s && docker push artbio/metavisitor-2:$TRAVIS_COMMIT || echo "push failed twice, quitting" && exit 1)
+    docker push artbio/metavisitor-2:latest || (sleep 5s && docker push artbio/metavisitor-2:latest || echo "push failed twice, quitting" && exit 1)
+fi
